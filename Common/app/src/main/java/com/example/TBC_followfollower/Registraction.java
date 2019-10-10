21ba866670561e2888589b0dcb56a5ee72731793
reiji <rei0611kakoi@gmail.com>
2019-10-10 14:52:40 +0900

    会員登録1
    レイアウト（ボタン抜き）

---
+++ Common/app/src/main/java/com/example/TBC_followfollower/Registraction.java
@@ -0,0 +1,14 @@
+package com.example.TBC_followfollower;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registraction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Registraction);
+    }
+}
